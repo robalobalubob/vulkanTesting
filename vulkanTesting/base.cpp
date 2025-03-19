df032517f690e4eec25f753f6ef992b09aa5a95e
Robby Williams <liberator8461071@gmail.com>
2025-03-19 17:01:40 -0500

    Finished Render Passes

--- vulkanTesting/base.cpp
+++ vulkanTesting/base.cpp
@@ -96,6 +96,8 @@ private:
     VkExtent2D swapChainExtent;
 	std::vector<VkImageView> swapChainImageViews;
 
+	// Pipeline variables
+	VkRenderPass renderPass;
     VkPipelineLayout pipelineLayout;
 
 
@@ -128,6 +130,7 @@ private:
 
     void cleanup() {
         vkDestroyPipelineLayout(device, pipelineLayout, nullptr);
+		vkDestroyRenderPass(device, renderPass, nullptr);
 		for (auto imageView : swapChainImageViews) {
 			vkDestroyImageView(device, imageView, nullptr);
 		}
@@ -380,6 +383,21 @@ private:
         colorAttachmentRef.attachment = 0;
         colorAttachmentRef.layout = VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL;
 
+		VkSubpassDescription subpass{};
+		subpass.pipelineBindPoint = VK_PIPELINE_BIND_POINT_GRAPHICS;
+        subpass.colorAttachmentCount = 1;
+		subpass.pColorAttachments = &colorAttachmentRef;
+
+		VkRenderPassCreateInfo renderPassInfo{};
+		renderPassInfo.sType = VK_STRUCTURE_TYPE_RENDER_PASS_CREATE_INFO;
+		renderPassInfo.attachmentCount = 1;
+		renderPassInfo.pAttachments = &colorAttachment;
+		renderPassInfo.subpassCount = 1;
+        renderPassInfo.pSubpasses = &subpass;
+
+		if (vkCreateRenderPass(device, &renderPassInfo, nullptr, &renderPass) != VK_SUCCESS) {
+			throw std::runtime_error("failed to create render pass!");
+		}
     }
 
 	void createGraphicsPipeline() {
